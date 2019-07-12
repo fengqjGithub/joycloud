ef6c88dd509652b462a374151bdec74e4177f8b4
DESKTOP-LCBDNCC\Administrator <409902978@qq.com>
2019-07-12 22:41:31 +0800

    初始化

---
+++ eureka-server/src/main/java/com/eurakeserver/EurakeServerApplication.java
@@ -0,0 +1,13 @@
+package com.eurakeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurakeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurakeServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/joycloud/JoycloudApplication.java
@@ -0,0 +1,13 @@
+package com.joycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoycloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoycloudApplication.class, args);
+	}
+
+}
