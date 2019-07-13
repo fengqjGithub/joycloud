8170980ebe1dd954b13176a272c81736f98580b9
fengqj <409902978@qq.com>
2019-07-13 12:03:46 +0800

    改配置

--- eureka-server/src/main/java/com/eurakeserver/EurakeServerApplication.java
+++ eureka-server/src/main/java/com/eurakeserver/EurakeServerApplication.java
@@ -2,12 +2,15 @@ package com.eurakeserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurakeServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurakeServerApplication.class, args);
+		System.out.println("-----server 启动成功---------");
 	}
 
 }
