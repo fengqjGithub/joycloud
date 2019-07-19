ef82d98d11bab32b9d8838b819de6a9475645069
DESKTOP-LCBDNCC\Administrator <409902978@qq.com>
2019-07-19 22:42:13 +0800

    feign-消费者

---
+++ consumer-feign/src/main/java/com/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.controller;
+
+import com.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Project: joycloud
+ * @Package: com.controller
+ * @Author: 冯前进
+ * @Date: 2019-07-19 19:46
+ * @Description: TODO
+ **/
+@RestController
+public class TestController {
+    @Autowired
+    TestService testService;
+
+    @GetMapping(value = "/consumerHi")
+    public String sayHi(@RequestParam(value = "msg") String msg) {
+        return testService.consumerFeignSayHi(msg);
+    }
+}
---
+++ consumer-feign/src/main/java/com/service/TestService.java
@@ -0,0 +1,20 @@
+package com.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Project: joycloud
+ * @Package: com.service
+ * @Author: 冯前进
+ * @Date: 2019-07-19 19:43
+ * @Description: TODO
+ **/
+@FeignClient(value = "service-hi")
+public interface TestService {
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    String consumerFeignSayHi(@RequestParam(value = "msg") String msg);
+
+}
