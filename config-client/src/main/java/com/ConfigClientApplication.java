9fc6a03128a7dd0ddec78cf7b3052fcd256da8ba
DESKTOP-LCBDNCC\Administrator <409902978@qq.com>
2019-07-27 23:50:46 +0800

    添加消息总线springcloud-bus，配置rabbitmq

--- config-client/src/main/java/com/ConfigClientApplication.java
+++ config-client/src/main/java/com/ConfigClientApplication.java
@@ -2,16 +2,20 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 /**
+ * 注意：@RefreshScope 注解需要添加到读取配置的类上
+ *
  * 1：客户端的spring.application.name配置config-clent是和Git服务器上面的文件名相对应的，
  * 如果你的客户端是其他名字就报错找不到参数。
  * Git上面是有这个config-client-dev的配置文件的所以是config-clent，
